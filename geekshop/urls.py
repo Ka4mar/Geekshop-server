9967c606d03beeb66e47e552ef702b1eb28c3b6b
Ka4mar <Berserk_ka4mar@mail.ru>
2021-06-28 12:29:59 +0300

    added dynamic static

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from django.contrib import admin
-from django.urls import path
 
 from products.views import index, products
 
