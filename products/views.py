5b88a8145c005490a5c9589d441551ecd221ebc9
Ka4mar <Berserk_ka4mar@mail.ru>
2021-07-01 14:29:23 +0300

    added models for products and added migration file products

--- products/views.py
+++ products/views.py
@@ -1,9 +1,13 @@
 import datetime
+import os
+import json
 
 from django.shortcuts import render
 
 # Create your views here.
 # Функции = контроллер = вьюхи
+
+MODULE_DIR = os.path.dirname(__file__)
 
 
 def index(request):
@@ -18,13 +22,8 @@ def index(request):
 def products(request):
     context = {
         'title': 'GeekShop - Каталог',
-        'products': [
-            {'img': '/static/vendor/img/products/Adidas-hoodie.png', 'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090, 'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'},
-            {'img': '/static/vendor/img/products/Blue-jacket-The-North-Face.png', 'name': 'Синяя куртка The North Face', 'price': 23725, 'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'},
-            {'img': '/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png', 'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390, 'description': 'Материал с плюшевой текстурой. Удобный и мягкий.'},
-            {'img': '/static/vendor/img/products/Black-Nike-Heritage-backpack.png', 'name': 'Черный рюкзак Nike Heritage', 'price': 2340, 'description': 'Плотная ткань. Легкий материал.'},
-            {'img': '/static/vendor/img/products/Black-Dr-Martens-shoes.png', 'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': 13590, 'description': 'Гладкий кожаный верх. Натуральный материал.'},
-            {'img': '/static/vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png', 'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': 2890, 'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.'},
-        ]
     }
+    file_path = os.path.join(MODULE_DIR, 'fixtures/goods.json')
+    context['products'] = json.load(open(file_path, encoding='utf-8 '))
+
     return render(request, 'products/products.html', context)
