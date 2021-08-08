b990a00c51aad8d715815c82671a209296d28a7c
Ka4mar <Berserk_ka4mar@mail.ru>
2021-08-08 17:21:12 +0300

    Create CRUD controllers for Django CBV-based order

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,7 +1,12 @@
+from django.db import transaction
+from django.forms import inlineformset_factory
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
-from ordersapp.models import Order
+from baskets.models import Basket
+from ordersapp.forms import OrderItemForm
+from ordersapp.models import Order, OrderItem
 
 
 class OrderList(ListView):
@@ -10,3 +15,41 @@ class OrderList(ListView):
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
 
+
+class OrderItemCreate(CreateView):
+    model = Order
+    fields = []
+    success_url = reverse_lazy('ordersapp:order_list')
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        OrderFormset = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=1)
+
+        if self.request.method == 'POST':
+            formset = OrderFormset(self.request.POST)
+        else:
+            basket_items = Basket.objects.filter(user=self.request.user)
+            if basket_items.exists():
+                OrderFormset = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=basket_items.count())
+                formset = OrderFormset()
+                for num, form in enumerate(formset.forms):
+                    form.initial['product'] = basket_items[num].product
+                    form.initial['quantity'] = basket_items[num].quantity
+            else:
+                formset = OrderFormset()
+
+        data['orderitems'] = formset
+        return data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        orderitems = context['orderitems']
+
+        with transaction.atomic():
+            form.instance.user = self.request.user
+            self.object = form.save()
+            if orderitems.is_valid():
+                orderitems.instance = self.object
+                orderitems.save()
+
+        return super().form_valid(form)
