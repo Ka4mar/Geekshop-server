85cb48daa871426eb22c8fa51c1732691b26d324
Ka4mar <Berserk_ka4mar@mail.ru>
2021-08-08 15:16:04 +0300

    Create a user order application

---
+++ ordersapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from ordersapp import views
+
+app_name = 'ordersapp'
+
+
+urlpatterns = [
+    path('', views.OrderList.as_view(), name='order_list'),
+    path('create/', views.OrderList.as_view(), name='order_create'),
+    path('read/<pk>/', views.OrderList.as_view(), name='order_read'),
+    path('update/<pk>/', views.OrderList.as_view(), name='order_update'),
+    path('delete/<pk>/', views.OrderList.as_view(), name='order_delete'),
+]
